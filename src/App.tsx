ebbf933b42ca50a18dfd1e1040d24b5a7df0b0f0
Paul.Dempsey <paul.daniel.dempsey@icloud.com>
2022-04-11 14:37:00 +0100

    Act1: asynce/await axios get, useEffect/useState initial load

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
 
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/header';
 import CharacterContainer from './components/character_container';
 import Navigation from './components/navigation';
 import { DisneyCharacter } from './disney_character';
+import axios from 'axios';
 
 const App : React.FC = () => {
 
 	const [currentPage, setCurrentPage] = useState<number>(1);
 
   // Some dummy state representing disney characters
-  const [characters, setCharacters] = useState<Array<DisneyCharacter>>([
-    {
-      _id: 6,
-      name: "'Olu Mel",
-      imageUrl: "https://static.wikia.nocookie.net/disney/images/6/61/Olu_main.png"
-    },
-    {
-      _id: 25,
-      name: "Abu",
-      imageUrl: "https://static.wikia.nocookie.net/disney/images/3/3f/Profile_-_Abu.png"
-    },
-    {
-      _id: 30,
-      name: "Ace",
-      imageUrl: "https://static.wikia.nocookie.net/disney/images/1/1e/Profile_-_Ace.png"
-    },
-  ]);
+  const [characters, setCharacters] = useState<Array<DisneyCharacter>>([]);
 
-  
+  useEffect(() => {
+    getCharacters(1);
+  }, []);
+
+  const getCharacters = async (pageNumber: number) => {
+    const apiResponse = await axios.get(`https://api.disneyapi.dev/characters?page=${pageNumber}`);
+    setCharacters(apiResponse.data.data);
+  };
 
   return (
     <div className="page">
